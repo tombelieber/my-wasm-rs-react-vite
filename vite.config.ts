00ddaf3fdc1a1a3e1391a90cf4ecd0a57624a583
Tom Tang <jjjj1997@live.hk>
2025-03-28 03:20:03 +0800

    Update Vite configuration to target ESNext for improved compatibility with top-level await

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import wasm from "vite-plugin-wasm";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), wasm()],
+    build: {
+        target: "esnext", // allow top-level await
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "esnext",
+        },
+    },
 });
